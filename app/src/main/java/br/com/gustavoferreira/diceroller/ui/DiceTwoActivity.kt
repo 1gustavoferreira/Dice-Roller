71ff278bc6a104077222df1a9a9a7a2e004837b5
soldng <gustavoferreira0gu@gmail.com>
2022-06-15 10:38:36 -0300

    created the d20 screen and finished

---
+++ app/src/main/java/br/com/gustavoferreira/diceroller/ui/DiceTwoActivity.kt
@@ -0,0 +1,11 @@
+package br.com.gustavoferreira.diceroller.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiceTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dice_two)
+    }
+}
